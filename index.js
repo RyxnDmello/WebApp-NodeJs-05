a4a777d88519e6754da988717fb5ba610f62fe23
RYAN NOLASCO D MELLO <ryanndmello10@gmail.com>
2023-07-10 02:36:53 +0530

    CyberVerse V1.0.0

---
+++ index.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static("./JavaScript"));
+app.use(express.static("./images"));
+app.use(express.static("./json"));
+app.use(express.static("./css"));
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.listen(process.env.DEVELOPMENT_PORT, () => {
+  console.log(`ACTIVE | PORT: ${process.env.DEVELOPMENT_PORT}`);
+});
