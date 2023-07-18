58cb33b042ad6d0c64d52b93f1f8fc3572e9b08d
RYAN NOLASCO D MELLO <ryanndmello10@gmail.com>
2023-07-19 00:58:22 +0530

    CyberVerse V1.1.7

--- JavaScript/home/games/GamesCarousel.js
+++ JavaScript/home/games/GamesCarousel.js
@@ -10,12 +10,54 @@ function CarouselController() {
   buttons.forEach((button, index) => {
     button.addEventListener("click", () => {
       CarouselMoveSlides(index);
+      ControllerAnimation(index);
     });
   });
 }
 
 function CarouselMoveSlides(index) {
-  slides.forEach((slide) => {
-    slide.style.transform = `translateX(${-index * carousel.clientWidth}px)`;
+  CarouselAnimation(0, 100);
+
+  setTimeout(() => {
+    slides.forEach((slide) => {
+      slide.style.transform = `translateX(${-index * carousel.clientWidth}px)`;
+    });
+  }, 700);
+
+  CarouselAnimation(800, 100);
+}
+
+function ControllerAnimation(index) {
+  buttons.forEach((button, currentIndex) => {
+    if (currentIndex === index) {
+      button.style.translate = "0 -10px";
+      button.style.opacity = 1;
+    } else {
+      button.style.translate = "0 0";
+      button.style.opacity = 0.25;
+    }
   });
+}
+
+function CarouselAnimation(duration, delay) {
+  setTimeout(() => {
+    slides.forEach((slide) => {
+      const games = slide.querySelectorAll(".games-slide-title, .game");
+      let delayDuration = 0;
+
+      games.forEach((game) => {
+        setTimeout(() => {
+          if (game.classList.contains("game-fade-out")) {
+            game.classList.remove("game-fade-out");
+            game.classList.add("game-fade-in");
+          } else {
+            game.classList.remove("game-fade-in");
+            game.classList.add("game-fade-out");
+          }
+        }, delayDuration);
+
+        delayDuration += delay;
+      });
+    });
+  }, duration);
 }
