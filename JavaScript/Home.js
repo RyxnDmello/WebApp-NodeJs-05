ee9c8881140b67237929a9a512d614ab43be71a6
RYAN NOLASCO D MELLO <ryanndmello10@gmail.com>
2023-07-30 03:12:44 +0530

    CyberVerse V1.4.2

--- JavaScript/Home.js
+++ JavaScript/Home.js
@@ -1,4 +1,4 @@
-import ProductCarousel from "./home/header/ProductCarousel.js";
+import HeaderCarousel from "./home/header/HeaderCarousel.js";
 import FeaturesCarousel from "./home/features/FeaturesCarousel.js";
 import GamesCarousel from "./home/games/GamesCarousel.js";
 import DetailsCarousel from "./home/details/DetailsCarousel.js";
@@ -6,7 +6,7 @@ import DetailsPointsCarousel from "./home/details/DetailsPointsCarousel.js";
 import ExclusiveCarousel from "./home/exclusive/ExclusiveCarousel.js";
 import CommentsCarousel from "./home/comments/CommentsCarousel.js";
 
-ProductCarousel();
+HeaderCarousel();
 FeaturesCarousel();
 GamesCarousel();
 
--- JavaScript/home/header/HeaderCarousel.js
+++ JavaScript/home/header/HeaderCarousel.js
@@ -1,13 +1,14 @@
 const carousel = document.querySelector(".header-carousel");
 const slides = document.querySelectorAll(".header-carousel-slide");
+
 const buttons = document.querySelectorAll(".header-carousel-button");
 const indicators = document.querySelectorAll(".header-carousel-indicator");
 
 const carouselWidth = carousel.clientWidth - 32;
 let currentSlide = 0;
 
-export default function ProductCarousel() {
-  buttons.forEach((button) => {
+export default function HeaderCarousel() {
+  buttons.forEach((button, index) => {
     button.addEventListener("click", () => {
       CarouselController(button);
       IndicatorAnimation(currentSlide);
@@ -25,9 +26,15 @@ export default function ProductCarousel() {
 }
 
 function CarouselMoveSlides() {
-  slides.forEach((slide) => {
-    slide.style.translate = `${-currentSlide * carouselWidth}px 0`;
-  });
+  CarouselAnimation(0, 137.5);
+
+  setTimeout(() => {
+    slides.forEach((slide) => {
+      slide.style.translate = `${-currentSlide * carouselWidth}px 0`;
+    });
+  }, 825);
+
+  CarouselAnimation(830, 137.5);
 }
 
 function CarouselController(button) {
@@ -36,6 +43,32 @@ function CarouselController(button) {
 
   if (currentSlide > 2) currentSlide = 2;
   if (currentSlide < 0) currentSlide = 0;
+}
+
+function CarouselAnimation(duration, delay) {
+  setTimeout(() => {
+    slides.forEach((slide) => {
+      const objects = slide.querySelectorAll(
+        ".header-carousel-slide-title, .header-product"
+      );
+
+      let delayDuration = 0;
+
+      objects.forEach((object) => {
+        setTimeout(() => {
+          if (object.classList.contains("header-product-fade-out")) {
+            object.classList.add("header-product-fade-in");
+            object.classList.remove("header-product-fade-out");
+          } else {
+            object.classList.add("header-product-fade-out");
+            object.classList.remove("header-product-fade-in");
+          }
+        }, delayDuration);
+
+        delayDuration += delay;
+      });
+    });
+  }, duration);
 }
 
 function IndicatorAnimation(selectedIndex) {
