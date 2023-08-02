4d9c2bdf8dc92f320e5209728afe8174fc79cc2f
RYAN NOLASCO D MELLO <ryanndmello10@gmail.com>
2023-08-02 15:32:41 +0530

    CyberVerse V1.4.8

--- JavaScript/register/DetailsBackground.js
+++ JavaScript/register/DetailsBackground.js
@@ -1,10 +1,13 @@
 const backgrounds = document.querySelectorAll(".register-details-background");
 const buttons = document.querySelectorAll(".details-controller-button");
+const indicators = document.querySelectorAll(".details-controller-indicator");
 
 let currentBackground = 0;
 
 export default function DetailsBackground() {
   BackgroundController();
+  IndicatorsController();
+  BackgroundSlideshow();
 }
 
 function BackgroundController() {
@@ -16,13 +19,50 @@ function BackgroundController() {
       if (index === 0) --currentBackground;
       if (index === 1) ++currentBackground;
 
-      backgrounds.forEach((background, backgroundIndex) => {
-        if (backgroundIndex === currentBackground) {
-          background.style.opacity = "1";
-        } else {
-          background.style.opacity = "0";
-        }
-      });
+      SwitchBackgrounds();
+      IndicatorAnimations();
     });
+  });
+}
+
+function IndicatorsController() {
+  indicators.forEach((indicator, index) => {
+    indicator.addEventListener("click", () => {
+      if (currentBackground === index) return;
+      currentBackground = index;
+
+      SwitchBackgrounds();
+      IndicatorAnimations();
+    });
+  });
+}
+
+function BackgroundSlideshow() {
+  setInterval(() => {
+    if (currentBackground === backgrounds.length - 1) currentBackground = 0;
+    else ++currentBackground;
+
+    SwitchBackgrounds();
+    IndicatorAnimations();
+  }, 5000);
+}
+
+function IndicatorAnimations() {
+  indicators.forEach((indicator, indicatorIndex) => {
+    if (indicatorIndex === currentBackground) {
+      indicator.style.opacity = "1";
+    } else {
+      indicator.style.opacity = "0.25";
+    }
+  });
+}
+
+function SwitchBackgrounds() {
+  backgrounds.forEach((background, backgroundIndex) => {
+    if (backgroundIndex === currentBackground) {
+      background.style.opacity = "1";
+    } else {
+      background.style.opacity = "0";
+    }
   });
 }
