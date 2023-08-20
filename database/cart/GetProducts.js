789e14c0745db383988ff62d9fba5e9b2f37e886
RyxnDmello <ryanndmello10@gmail.com>
2023-08-20 11:39:36 +0530

    CyberVerse V1.7.4

--- database/cart/GetProducts.js
+++ database/cart/GetProducts.js
@@ -6,60 +6,84 @@ const XboxTemplate = require("../../json/xbox.json");
 
 module.exports.DatabaseGetProducts = async (email) => {
   const databaseAccount = await AccountModel.findOne({ email: email });
+  const cart = CreateCart(databaseAccount);
+  return cart;
+};
 
-  const cart = [];
+const CreateCart = (account) => {
+  const originals = [];
+  const skins = [];
+  const combos = [];
 
-  for (let i = 0; i < databaseAccount.cart.length; i++) {
-    const cartProduct = databaseAccount.cart[i];
+  if (account.cart.length === 0) return;
+
+  for (let i = 0; i < account.cart.length; i++) {
+    const cartProduct = account.cart[i];
 
     if (cartProduct.details.brand === "playstation") {
-      cart.push({
+      const product = {
         product: GetPlaystationProduct(cartProduct.ID, cartProduct.details),
         details: cartProduct.details,
         price: cartProduct.price,
-      });
+      };
+
+      if (product.details.subType === "originals") originals.push(product);
+      if (product.details.subType === "skins") skins.push(product);
+      if (product.details.subType === "combos") combos.push(product);
       continue;
     }
 
     if (cartProduct.details.brand === "xbox") {
-      cart.push({
+      const product = {
         product: GetXboxProduct(cartProduct.ID, cartProduct.details),
         details: cartProduct.details,
         price: cartProduct.price,
-      });
+      };
+
+      if (product.details.subType === "originals") originals.push(product);
+      if (product.details.subType === "skins") skins.push(product);
+      if (product.details.subType === "combos") combos.push(product);
       continue;
     }
 
     if (cartProduct.details.brand === "switch") {
-      cart.push({
+      const product = {
         product: GetSwitchProduct(cartProduct.ID, cartProduct.details),
         details: cartProduct.details,
         price: cartProduct.price,
-      });
+      };
+
+      if (product.details.subType === "originals") originals.push(product);
+      if (product.details.subType === "skins") skins.push(product);
+      if (product.details.subType === "combos") combos.push(product);
       continue;
     }
   }
 
-  return cart;
+  return {
+    originals: originals,
+    skins: skins,
+    combos: combos,
+  };
 };
 
-function GetPlaystationProduct(ID, details) {
+const GetPlaystationProduct = (ID, details) => {
   const products = PlaystationTemplate[details.type][details.subType].products;
   return FindAndGetProductByID(ID, products);
-}
+};
 
-function GetXboxProduct(ID, details) {
+const GetXboxProduct = (ID, details) => {
   const products = XboxTemplate[details.type][details.subType].products;
   return FindAndGetProductByID(ID, products);
-}
+};
 
-function GetSwitchProduct(ID, details) {
+const GetSwitchProduct = (ID, details) => {
   const products = SwitchTemplate[details.type][details.subType].products;
   return FindAndGetProductByID(ID, products);
-}
+};
 
-function FindAndGetProductByID(ID, products) {
+const FindAndGetProductByID = (ID, products) => {
   for (let i = 0; i < products.length; i++) {
     if (products[i].ID === ID) return products[i];
   }
-}
+};
